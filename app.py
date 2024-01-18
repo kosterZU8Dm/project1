26c42fce4c42ff096fdc89df17f22334314b2f0e
kosta_wsl <kosta_wsl@kostamail.com>
2024-01-19 00:33:55 +0300

    rm draft function and add correct feature of send2psql

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
@@ -21,6 +21,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER
 db = SQLAlchemy(app)
 ### POSTGRESQL ###
 
+class YourModel(db.Model):
+    __tablename__ = 'your_table'  # Замените 'your_table' на имя вашей таблицы
+
+    id = db.Column(db.Integer, primary_key=True)
+    text_column = db.Column(db.String(255))
+    number_column = db.Column(db.Integer)
+
+    def __init__(self, text_column, number_column):
+        self.text_column = text_column
+        self.number_column = number_column
+
 @app.route('/')
 def index():
     try:
@@ -41,11 +52,23 @@ def calculate():
         'texts': ', '.join(texts),
     }
 
-    return render_template('index.html', result=result)
+    return jsonify({'success': True, 'result': result})
 
-@app.route('/send2postgresql', methods=['POST'])
-def send2postgresql():
+@app.route('/send_to_postgres', methods=['POST'])
+def send_to_postgres():
+    texts = request.form.getlist('texts[]')
+    numbers = [int(num) for num in request.form.getlist('numbers[]')]
 
+    data_to_insert = [{'text_column': text, 'number_column': num} for text, num in zip(texts, numbers)]
+
+    try:
+        for data in data_to_insert:
+            db.session.add(YourModel(**data))
+
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'Данные успешно отправлены в PostgreSQL'})
+    except Exception as e:
+        return jsonify({'success': False, 'message': f'Ошибка при отправке данных в PostgreSQL: {str(e)}'})
 
 if __name__ == '__main__':
     with app.app_context(): # выполнение операций, требующих доступа к приложению.
